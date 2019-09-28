53ab65cb0cf1199ac771e02c97eed2e7b148ae14
MinenIntros <50350783+MinenIntros@users.noreply.github.com>
2019-09-28 10:58:14 +0200

    1.2.4

--- mailcow-installer.sh
+++ mailcow-installer.sh
@@ -44,6 +44,7 @@ function grayMessage() {
     echo ""
     magentaMessage "1.  Start installer"
     magentaMessage "2.  Start uninstaller"
+    echo ""
     magentaMessage "3.  Leave installer"
     echo ""
     whiteMessage "______________________________________________________"
